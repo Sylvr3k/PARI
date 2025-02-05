eb38780b09566f247c33b11f70f6997ffbfe4429
Sylvester <sangoshadrackkamenya@gmail.com>
2025-02-05 22:05:32 +0300

    backend HASHED ID

--- backend/models/User.js
+++ backend/models/User.js
@@ -24,6 +24,12 @@ userSchema.pre('save', async function (next) {
   if (this.isModified('password')) {
     this.password = await bcrypt.hash(this.password, 12);
   }
+
+      
+  if (this.isModified('id')) {
+    this.id = await bcrypt.hash(this.id, 12);
+  }
+  
   next();
 });
 
